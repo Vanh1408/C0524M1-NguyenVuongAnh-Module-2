6fa92260d8c4b4b2bbd83fff96be4eba8a31abfc
Vanh1408 <vanh0329855800@gmail.com>
2024-08-05 20:56:31 +0700

    hien thi loi chao

---
+++ introduction_to_java/src/hien_thi_loi_chao/Main.java
@@ -0,0 +1,12 @@
+package hien_thi_loi_chao;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
