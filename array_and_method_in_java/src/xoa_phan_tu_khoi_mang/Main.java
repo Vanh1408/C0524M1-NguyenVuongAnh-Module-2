fe3f62f8ea0542c37e495f307ff5da450424988b
Vanh1408 <vanh0329855800@gmail.com>
2024-08-10 18:24:03 +0700

    xoa phan tu khoi mang

--- array_and_method_in_java/src/xoa_phan_tu_khoi_mang/Main.java
+++ array_and_method_in_java/src/xoa_phan_tu_khoi_mang/Main.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, i, l;
-        do{
-            System.out.println("Input array length: ");
-            n = sc.nextInt();
-        }while(n <= 0);
-        int[] arr = new int[n];
-        for (i = 0; i < n; i++){
+        System.out.println("Input array length: ");
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++){
             System.out.println("Input array element: ");
             arr[i] = sc.nextInt();
         }
         System.out.println("Input element x: ");
         int x = sc.nextInt();
-        for (l = i = 0; i < n; i++){
-            if(arr[i] != x){
-                arr[l] = arr[i];
-                l++;
+        int index_del = -1;
+        for (int i = 0; i < N; i++){
+            if(arr[i] == x){
+                index_del = i;
+                break;
             }
         }
-        n = l;
-        System.out.println("Array after delete element x: ");
-        for (i = 0; i < n; i++){
-            System.out.print(arr[i] + " ");
+        if (index_del != -1){
+            for (int i = index_del; i < N - 1; i++){
+                arr[i] = arr[i+1];
+            }
+            N--;
+            System.out.println("Array after delete element x: ");
+            for (int i = 0; i < N; i++){
+                System.out.print(arr[i] + " ");
+            }
         }
     }
 }
