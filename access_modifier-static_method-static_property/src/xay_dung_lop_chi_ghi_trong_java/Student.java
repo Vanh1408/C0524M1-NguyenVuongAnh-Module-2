a5a538d0fa445a1a3ffd68b7394924280893f1de
Vanh1408 <vanh0329855800@gmail.com>
2024-08-16 18:55:01 +0700

    xay dung lop chi ghi trong java

---
+++ access_modifier-static_method-static_property/src/xay_dung_lop_chi_ghi_trong_java/Student.java
@@ -0,0 +1,17 @@
+package xay_dung_lop_chi_ghi_trong_java;
+
+class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ access_modifier-static_method-static_property/src/xay_dung_lop_chi_ghi_trong_java/Test.java
@@ -0,0 +1,10 @@
+package xay_dung_lop_chi_ghi_trong_java;
+
+class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        s2.setName("Vanh");
+        s2.setClasses("C05");
+    }
+}
